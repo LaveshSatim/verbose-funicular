f7cc3801409f770b052606b18a319f02a00a07da
Lavesh Satim <lavesh.git@gmail.com>
2022-12-23 22:36:53 +0530

    xx

--- ch1/sandbox.js
+++ ch1/sandbox.js
@@ -1,4 +1,34 @@
 let a = 10;
 let b = 20;
 
-alert(a + "" + b + " saa `${a}`" + (a + b));
+
+// alert(`${a}`+"  "+b);
+
+//function declaration
+
+function sayHallo(name) {
+    alert("hello "+name);
+}
+
+sayHallo("lavesh");
+
+if(typeof a !== "string"){
+    console.log(a+ " is string");
+}
+
+//function expression
+const speak = function(name="'not passed'") {
+
+    console.log("name : "+name);
+
+};
+
+const play = (name) => {
+
+    console.log("play : "+name);
+
+}
+
+speak()
+
+play("teju")
